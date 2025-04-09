bd3122506c18e1b2d3f437dbb68439b296b19bea
naveen12-github <naveencse4321@gmail.com>
2025-04-09 21:16:10 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    
+      <>
+      <h1 className='text-3xl bg-red-500 gap-3 p-3 h-2 w-4 my-8'>hellO</h1>
+      <Home/>
+      </>
+  )
+}
+
+export default App
